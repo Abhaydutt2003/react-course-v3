dd36f0bc106ae88a070fe83bd39f1eb2219073ab
Abhay Dutt <dutt.abhay@gmail.com>
2023-10-23 22:15:38 +0530

    learn useState object

--- 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,35 @@
+import React from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  //function to remove 1 person
+  const removePerson = (id) => {
+    let newPeople = people.filter((person) => {
+      return person.id != id;
+    }); 
+    setPeople(newPeople);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button onClick={() => removePerson(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button
+        className="btn"
+        style={{ marginTop: "2rem" }}
+        onClick={()=>setPeople([])}
+      >
+        clear
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
--- 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,36 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson]  = useState({
+    name:'peter',
+    age:24,
+    hobby:'read books'
+  })
+  let updatePerson = (event) => {
+    event.preventDefault();
+    let [username,userage,userHobby] = event.target;
+    setPerson({
+      name:username.value,
+      age:userage.value,
+      hobby:userHobby.value
+    })
+  };
+  return (
+    <>
+      <div>
+        <h1>{person.name}</h1>
+        <h2>{person.age}</h2>
+        <h3>{person.hobby}</h3>
+      </div>
+      <br />
+      <form onSubmit={updatePerson}>
+        <input type="text" name="username" placeholder="Enter new name" />
+        <input type="number" name="userage" placeholder="new age" />
+        <input type="text" name="userHobby" placeholder="new hobby" />
+        <button type="submit">update</button>
+      </form>
+    </>
+  );
 };
 
 export default UseStateObject;
